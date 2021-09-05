188c3773ff1f828f5b1e52851415e4ed9caabfdd
lyndonli2525 <85649591+lyndonli2525@users.noreply.github.com>
2021-09-05 12:00:29 -0500

    Add files via upload

--- sha3.cuh
+++ sha3.cuh
@@ -2,46 +2,55 @@
 
 #ifndef SHA3_H
 #define SHA3_H
-
 #include <stddef.h>
 #include <stdint.h>
 #include <cuda.h>
 
-
-
-
 #define ROTL64(x, y) (((x) << (y)) | ((x) >> (64 - (y))))
-typedef unsigned char BYTE;
 
-
+//SHA3 context 
 typedef struct {
-    union {                                 
-        uint8_t b[200];       //8 bit bytes             
-        uint64_t q[25];       //64 bit words              
-    } A;
-    int pt; 
-	int rsiz; 
-	int mdlen;                    
+	union {
+	  uint8_t b[200];		//8 bit bytes
+	  uint64_t q[25];		//64 bit words
+	}A;
+	int pt;
+	int rsiz;
+	int mdlen;
 } sha3_ctx;
 
+//Structure to hold messages
 typedef struct JOB {
 	size_t size;
 	uint8_t digest[64];
 	char * data;
 }JOB;
 
+//copies sha3 constants to host
 void pre_sha3();
+
+//Updates SHA3 context's state
 __device__ void sha3_transform(uint64_t st[25]);
 
+//Initiates SHA3 context
+__device__ void sha3_init(sha3_ctx *ctx);
 
-__device__ void sha3_init(sha3_ctx *ctx);    
+//Absorbs input blocks for SHA3
 __device__ void sha3_update(sha3_ctx *ctx, const void *data, size_t len);
-__device__ void sha3_final(sha3_ctx *ctx, void *md);    
 
+//Finalizes SHA3 hash output
+__device__ void sha3_final(sha3_ctx *ctx, void *md);
+
+//Runs SHA3 function across a single line
 __global__ void sha3(const void *in, int n, size_t inlen, void *md);
-void print_job(JOB **jobs, int num_jobs);
-void runJobs(JOB** jobs, int num_inputs);
-int getNumInp(const char *inpF);
 
+//Prints out SHA3 outputs
+void print_job(JOB **jobs, int num_jobs);
+
+//Runs SHA3 using GPU
+void runJobs(JOB** jobs, int num_inputs);
+
+//Gets number of inputs in file
+int getNumInp(const char *inpF);
 
 #endif
--- sha3test.cu
+++ sha3test.cu
@@ -1,63 +1,81 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "sha3.cuh"
 
-
-
-
-JOB ** gpu_Run(const char *inpF){
+//Funtion to run SHA3 function across input file
+void gpu_Run(const char *inpF){
   JOB **jobs = NULL;
   FILE *inp;
-  inp = fopen(inpF, "r"); // filename of your data file
-  size_t num_inputs = getNumInp(inpF);
-  cudaMallocManaged(&jobs, num_inputs * sizeof(JOB*));
-  int i = 0;
-  while (1) {
-    int size = 0;
-    char r = (char)fgetc(inp);
-    while (r!= '\n' && r != ' ' && !feof(inp)) { // read till , or EOF
-      size++;               // store in array
-      r = (char)fgetc(inp);
+  if(inp = fopen(inpF, "r")) { // filename of your data file
+    size_t num_inputs = getNumInp(inpF);
+    cudaMallocManaged(&jobs, num_inputs * sizeof(JOB*));
+    int i = 0;
+    while (1) {
+      int size = 0;
+      char r = (char)fgetc(inp);
+      while (r!= '\n' && r != ' ' && !feof(inp)) { // read till , or EOF
+        size++;
+        r = (char)fgetc(inp);
+      }
+      if (size!=0) {//create jobs with size of inputs
+        JOB *job = NULL;
+        cudaMallocManaged(&job, sizeof(JOB));
+        job->size = size;
+        jobs[i] = job;
+        size = 0;
+      }
+      if (feof(inp)) { // check again for EOF
+        break;
+      }
+      i++;
+    } 
+    rewind(inp);
+    printf("Number of inputs: %zd\n", num_inputs);
+    for(int i=0; i<num_inputs; i++) {
+      char *buf = NULL;
+      cudaMallocManaged(&buf,  jobs[i]->size * sizeof(char));
+      int count = 0;
+      char r = (char)fgetc(inp);
+      while (count < jobs[i]->size) {
+        buf[count] = r;// store characters in array
+        r = (char)fgetc(inp);
+        count++;
+      }
+        buf[count] = '\0';
+      if (feof(inp)) { // check again for EOF
+        break;
+      }
+      jobs[i]->data = buf;
     }
-    if (size!=0) {
-      JOB *job = NULL;    
-      cudaMallocManaged(&job, sizeof(JOB));
-      job->size = size;
-      jobs[i] = job;
-      size = 0;
-    }
-    if (feof(inp)) { // check again for EOF
-      break;
-    }
-    i++;
-  } 
-  rewind(inp);
-  printf("Number of inputs: %zd\n", num_inputs);
-  for(int i=0; i<num_inputs; i++) {
-    char *buf = NULL;   
-    cudaMallocManaged(&buf,  jobs[i]->size * sizeof(char));
-    int count = 0;
-    char r = (char)fgetc(inp);
-    while (count < jobs[i]->size) {
-      buf[count] = r;               // store in array
-      r = (char)fgetc(inp);
-      count++;
-    }
-      buf[count] = '\0';
-    if (feof(inp)) { // check again for EOF
-      break;
-    }
-    jobs[i]->data = buf;
+
+    //Prepare the SHA3 constants
+    pre_sha3();
+
+    //Generate hash outputs
+    runJobs(jobs, num_inputs);
+
+    //Synchronize the threads
+    cudaDeviceSynchronize();
+
+    //Print out the hash outputs
+    print_job(jobs, num_inputs);
+
+    //Remove device allocations
+    cudaDeviceReset();
   }
-  pre_sha3();
-  runJobs(jobs, num_inputs);
-  cudaDeviceSynchronize();
-  print_job(jobs, num_inputs);
-  cudaDeviceReset();
-  return jobs;
+  else {
+    printf("File doesn't exist.");
+  }
 }
 int main(int argc, char **argv)
 {
-        JOB** jobs = gpu_Run("test2.txt");
-        return 0;
+  //ask for filename
+  char str[20];
+  printf("Please enter the filename:\n");
+  scanf("%s", str);
+
+  //run sha3 in cuda
+  gpu_Run(str);
+  return 0;
 }
